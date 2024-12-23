b48520b30ab2eaabd37f21fb759e631309bf47b3
tanakana8 <nkhshc421035@g.neec.ac.jp>
2024-12-23 15:54:42 +0900

    6-4-3

---
+++ 6-4/6-4-3.mjs
@@ -0,0 +1,17 @@
+const score = 59
+if(score>=90)
+{
+    console.log("Exellent")
+}
+else if(score>=80)
+{
+    console.log("Good")
+}
+else if(score>=60)
+{
+    console.log("Normal")
+}
+else if(score<60)
+{
+    console.log("Red")
+}
