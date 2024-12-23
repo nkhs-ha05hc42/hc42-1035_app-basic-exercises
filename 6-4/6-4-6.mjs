a72808570217fb2978b2c6d641978aad12b86415
tanakana8 <nkhshc421035@g.neec.ac.jp>
2024-12-23 15:54:59 +0900

    6-4-6

---
+++ 6-4/6-4-6.mjs
@@ -0,0 +1,13 @@
+const place = "ウポポイ"
+switch(place){
+    case "地獄谷":
+    case "大湯沼":
+        console.log("登別")
+        break
+    case "ウポポイ":
+    case "白老牛":
+        console.log("白老")
+        break
+    default:
+        console.log("対象外")
+}
