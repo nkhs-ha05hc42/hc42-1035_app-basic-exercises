b046209d278b2661bbf78b9f79de08d4a55eb1db
tanakana8 <nkhshc421035@g.neec.ac.jp>
2025-01-26 22:01:06 +0900

    7-2-3

---
+++ 7-2/7-2-3.mjs
@@ -0,0 +1,7 @@
+import {kakeru,waru} from "./7-2-3_export.mjs"
+
+const result = kakeru(1,2)
+console.log(result)
+
+const result1 = waru(4,2)
+console.log(result1)
---
+++ 7-2/7-2-3_export.mjs
@@ -0,0 +1,7 @@
+export const kakeru = (a,b) => {
+    return a*b
+}
+
+export const waru = (a,b) => {
+    return a/b
+}
