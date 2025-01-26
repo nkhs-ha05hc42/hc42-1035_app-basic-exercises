33a8b823897262df5e928b174ce988c386a619db
tanakana8 <nkhshc421035@g.neec.ac.jp>
2025-01-26 22:01:32 +0900

    7-4-1

---
+++ 7-4/7-4-1.mjs
@@ -0,0 +1,8 @@
+console.log("start",new Date())
+setTimeout(() => {
+    console.log("★",new Date())
+},10000)
+console.log("end",new Date())
+setTimeout(() => {
+    console.log("◆",new Date())
+},20000)
